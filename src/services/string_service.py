f7f6b247c9295314b7289cedb1795ea323dd7915
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-11-07 11:44:22 +0200

    StringService-luokka trie-tietorakenteen käyttämiseen

--- src/services/string_service.py
+++ src/services/string_service.py
@@ -1,13 +1,35 @@
-## Ideana toteuttaa luokka, joka vastaa sovelluslogiikasta
-## soveltaen trie-tietorakennetta ja Damerau Levenshtein -etäisyyttä
-## lisätäkseen, hakeakseen ja verratakseen annettua merkkijonoa
+from services.trie import Trie
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+TEXT_FILE = ROOT_DIR / "words.txt"
 
 class StringService:
-    def __init__(self):
-        pass
+    """Luokka, joka käsittelee käyttöliittymän syötteitä ja lisää sanoja trie-tietorakenteeseen.
 
+    Attributes:
+        trie (Trie): Trie-luokan ilmentymä, jota käytetään sanojen tallentamiseen ja hallintaan.
+    """
+    def __init__(self):
+        self.trie = Trie()
+        
+    def add_file_words_to_trie(self):
+        with open(TEXT_FILE) as words_file:
+            for word in words_file:
+                word = word.strip()
+                self.trie.add_word(word)
+    
+    def search_word_from_trie(self, word):
+        if self.trie.search_word(word):
+            return True
+        else:
+            return False
+    
     def create_string(self, string):
-        print(string)
-        return string
+        string = string.strip()
+        if len(string) > 0:
+            self.trie.add_word(string)
+        return False
 
 string_service = StringService()
+    
