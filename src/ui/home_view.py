105858e1f921899f7b5c5181f168e474cd92b4d9
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-11-07 11:54:58 +0200

    customtkinterin lisäystä

--- src/ui/home_view.py
+++ src/ui/home_view.py
@@ -1,4 +1,5 @@
-from tkinter import ttk, constants
+from tkinter import constants
+import customtkinter
 
 class HomeView:
     def __init__(self, root, handle_add_string, handle_search_string):
@@ -10,27 +11,31 @@ class HomeView:
         self._initialize()
 
     def pack(self):
-        self._frame.pack(fill=constants.X)
+        self._frame.pack(fill=constants.BOTH, expand=True)
     
     def destroy(self):
         self._frame.destroy()
     
     def _initialize(self):
-        self._frame = ttk.Frame(master=self._root)
-        label = ttk.Label(master=self._frame, text="Home")
+        self._frame = customtkinter.CTkFrame(master=self._root)
+        label = customtkinter.CTkLabel(master=self._frame, text="Welcome!")
 
-        add_button = ttk.Button(
+        self._frame.grid_rowconfigure(0, weight=1)
+        self._frame.grid_columnconfigure(0, weight=1)
+
+        add_button = customtkinter.CTkButton(
             master=self._frame,
-            text="Add a new string",
+            text="Add a new word",
             command=self._handle_add_string
         )
 
-        search_button = ttk.Button(
+        search_button = customtkinter.CTkButton(
             master=self._frame,
-            text="Search string",
+            text="Search a word",
             command=self._handle_search_string
         )
 
-        label.grid(row=0, column=0)
-        add_button.grid(row=1, column=0)
-        search_button.grid(row=1, column=1)
+        label.grid(row=0, column=0, pady=(20, 10), sticky="n")
+        add_button.grid(row=1, column=0, padx=20, pady=10, sticky="ew")
+        search_button.grid(row=2, column=0, padx=20, pady=(10, 20), sticky="ew")
+
