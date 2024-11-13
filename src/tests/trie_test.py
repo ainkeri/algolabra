064feb3e11eb7e7c2e258c367927d9848b1188f0
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-11-13 17:12:29 +0200

    Testien rakenteen muokkaus ja pylinttiä

--- src/tests/trie_test.py
+++ src/tests/trie_test.py
@@ -1,22 +1,10 @@
 import unittest
 from services.trie import Trie
 
+
 class TestTrie(unittest.TestCase):
     def setUp(self):
         self.trie = Trie()
-
-    def test_a_word_is_added_to_trie_and_found(self):
-        word = "testword"
-        self.trie.add_word(word)
-
-        self.assertEqual(self.trie.search_word(word), True)
-
-    def test_a_word_not_in_trie_is_not_found(self):
-        word = "notintrie"
-
-        self.assertEqual(self.trie.search_word(word), False)
-    
-    def test_str_is_returned_correctly_with_dfs(self):
         self.trie.add_word("metsänomistaja")
         self.trie.add_word("normaaliolot")
         self.trie.add_word("musiikkinäytelmä")
@@ -28,20 +16,16 @@ class TestTrie(unittest.TestCase):
         self.trie.add_word("metsänparannus")
         self.trie.add_word("normaalinäköinen")
 
+    def test_str_is_returned_correctly_with_dfs(self):
         self.trie.get_all_words()
 
-        self.assertEqual(self.trie.__str__(), "metsänomistaja, metsänomistus, metsänparannus, musiikkinäytelmä, musiikkiopinnot, musiikkiopisto, normaaliolot, normaaliobjektiivi, normaalipaino, normaalinäköinen")
-    
-    def test_get_all_words(self):
-        self.trie.add_word("miuku")
-        self.trie.add_word("mauku")
+        self.assertEqual(self.trie.__str__(
+        ), "metsänomistaja, metsänomistus, metsänparannus, musiikkinäytelmä, musiikkiopinnot, musiikkiopisto, normaaliolot, normaaliobjektiivi, normaalipaino, normaalinäköinen")
 
-        all_words = self.trie.get_all_words()
+    def test_search_word(self):
+        self.assertTrue(self.trie.search_word("normaaliolot"))
+        self.assertFalse(self.trie.search_word("nothere"))
 
-        self.assertEqual(all_words, ["miuku", "mauku"])
-    
-    
-
-        
-
-
+    def test_add_word(self):
+        self.trie.add_word("newword")
+        self.assertTrue(self.trie.search_word("newword"))
