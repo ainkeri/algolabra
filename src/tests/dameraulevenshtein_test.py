2d7217db1f9cb508c60d26ea479f9a4ee6a71851
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-11-13 17:10:29 +0200

    Testien pientä muokkausta

--- src/tests/dameraulevenshtein_test.py
+++ src/tests/dameraulevenshtein_test.py
@@ -1,32 +1,27 @@
 import unittest
 from services.dameraulevenshtein import DamerauLevenshtein
 
+
 class TestDamerauLevenshtein(unittest.TestCase):
     def setUp(self):
         self.dl = DamerauLevenshtein()
-        self.word1 = "lapoi"
-        self.word2 = "lapsi"
-    
+        self.word = "lapoi"
+
     def test_words_compared_have_distance_one(self):
-        self.assertEqual(self.dl.edit_distance(self.word1, self.word2), 1)
-    
+        self.assertEqual(self.dl.edit_distance(self.word, "lapsi"), 1)
+
     def test_words_compared_have_distance_two(self):
-        self.assertEqual(self.dl.edit_distance(self.word1, "lapua"), 2)
-    
-    def test_words_compared_have_distance_two_again(self):
-        self.assertEqual(self.dl.edit_distance(self.word2, "lapua"), 2)
+        self.assertEqual(self.dl.edit_distance(self.word, "lapua"), 2)
 
     def test_words_compared_have_distance_three(self):
-        self.assertEqual(self.dl.edit_distance(self.word2, "virsi"), 3)
-    
-    def test_words_given_have_correct_distance(self):
-        self.assertEqual(self.dl.edit_distance("viri", "virsi"), 1)
-    
+        self.assertEqual(self.dl.edit_distance(self.word, "varsi"), 3)
+
+    def test_words_compared_have_greater_edit_distance(self):
+        self.assertEqual(self.dl.edit_distance(
+            "oikeudenmukaisuus", "virsi"), 15)
+
     def test_one_empty_word_has_right_distance(self):
-        self.assertEqual(self.dl.edit_distance(self.word1, ""), 5)
-    
-    def test_one_empty_word_has_right_distance_again(self):
-        self.assertEqual(self.dl.edit_distance("", self.word2), 5)
-    
+        self.assertEqual(self.dl.edit_distance(self.word, ""), 5)
+
     def test_two_empty_words_has_right_distance(self):
         self.assertEqual(self.dl.edit_distance("", ""), 0)
