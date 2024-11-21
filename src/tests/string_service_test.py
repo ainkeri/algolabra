ad38a61621b8f64f0036daf7b66e21bfb471d447
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-11-21 19:36:01 +0200

    Lauseiden testausta

--- src/tests/string_service_test.py
+++ src/tests/string_service_test.py
@@ -25,6 +25,23 @@ class TestStringService(unittest.TestCase):
         self.assertEqual(self.string_service.__str__(),
                          "Tarkoititko: 'hirsi'?")
         self.assertFalse(False)
+    
+
+    def test_sentence_gets_a_suggestion(self):
+        sentence = "koira ruu kisra lapio"
+
+        self.string_service.search_word_from_trie(sentence)
+
+        self.assertEqual(self.string_service.__str__(),
+                         "Tarkoititko: 'koira juu kisa lapio'?")
+        self.assertFalse(False)
+
+    def test_correct_sentence_is_found(self):
+        sentence = "koira lapsi kissa lapio hiekka"
+
+        self.string_service.search_word_from_trie(sentence)
+
+        self.assertTrue(True)
 
     def test_word_too_complex_gets_no_suggestion(self):
         string = "slkdjskldjksjd"
